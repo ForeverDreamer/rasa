58450b52372cdfa7c5529e8b2180cab051b8da98
doer <499361328@qq.com>
2022-04-29 18:10:50 +0800

    新增清除会话记忆action，优化天气查询action

---
+++ examples/xiaoasheng/actions/weather.py
@@ -0,0 +1,25 @@
+from typing import Dict, Text, Any, List
+from rasa_sdk import Tracker, Action
+from rasa_sdk.events import Restarted
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionWeatherForm(Action):
+    def name(self) -> Text:
+        """Unique identifier of the form"""
+        return "action_weather_form"
+
+    def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Define what the form has to do
+        after all required slots are filled"""
+        address = tracker.get_slot('address')
+        date_time = tracker.get_slot('date_time')
+        print(f"action_weather_form->address:{address}")
+        print(f"action_weather_form->date_time:{date_time}")
+        dispatcher.utter_message("正在为你查询 {} {}的天气 ing".format(address, date_time))
+        return []
